4111d5574f3d6c343df4e7434cd502d91065f994
madhusudan9332 <madhusudangautam003@gmail.com>
2024-03-05 18:19:39 +0530

    add dom project files

---
+++ script.js
@@ -0,0 +1,8 @@
+let mydiv = document.querySelector("#container");
+mydiv.style.backgroundColor = "yellow";
+mydiv.style.height = "250px";
+mydiv.style.width = "250px";
+mydiv.style.padding = "25px";
+mydiv.style.margin = "25px";
+mydiv.style.fontSize = "25px" ;
+mydiv.style.fontWeight = "bold" ;
